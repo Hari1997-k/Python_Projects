796c574279e19d07c4986f6acfd89e269a28cf29
Hari K <61832395+Hari1997-k@users.noreply.github.com>
2020-07-10 18:15:28 +0530

    Update Whatsapp.py

--- Whatsapp.py
+++ Whatsapp.py
@@ -8,3 +8,13 @@ from_num = 'whatsapp:+14155238886'
 to_num = 'whatsapp:' + os.environ['My_Account_sid']
 
 client.messages.create(body='HELLO WORLD', from_=from_num, to=to_num)
+
+# ******************************** SIMPLIFIED WHATSAPP PROGRAM  ********************************
+# from twilio.rest import *
+# import os
+# from Python_Projects.Credentials import account_sid,auth_token,my_cell,my_twilio
+#
+# client = Client(account_sid, auth_token)
+# msg = "Hello Hiii"
+# res = client.messages.create(body=msg, from_=my_twilio, to= my_cell)
+#
